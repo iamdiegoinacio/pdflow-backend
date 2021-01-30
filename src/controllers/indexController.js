f865a36e96819a2af733e076ef6e1a610dae06e0
renatodev17 <pereirarenato21@gmail.com>
2021-01-30 11:39:25 -0300

    finalizing the integration of ESModules

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,6 +1,6 @@
 import { request as req, response as res } from 'express'
 
-module.exports = {
+export default {
   index(request = req, response = res) {
     return response.json({ message: 'This is an automatic PDF generation API created by Renato Pereira. Visit my Github: https://github.com/renatodev17/' })
   }
