02d7d8b83e31b771c44b958e4fe0a5d0cffbb434
renatodev17 <pereirarenato21@gmail.com>
2021-02-16 13:52:29 -0300

    Deleting all .pdf files from pdfs folder

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,6 +2,6 @@ import { request as req, response as res } from 'express'
 
 export default {
   index(request = req, response = res) {
-    return response.json({ message: 'This is an automatic PDF generation API created by Renato Pereira. Visit my Github: https://github.com/renatodev17/' })
+    return response.json({ message: 'This is an automatic PDF generation API created by Renato Pereira. Visit my Github: https://github.com/renato3x/' })
   }
 }
