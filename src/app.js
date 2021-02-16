67e8d340655bcdbd730b555b71cf55ea32d8a8b1
renatodev17 <pereirarenato21@gmail.com>
2021-02-16 20:05:22 -0300

    Creating a Service from Socket.io

--- src/app.js
+++ src/app.js
@@ -2,13 +2,18 @@ import express from 'express'
 import router from './router'
 import path from 'path'
 import cors from 'cors'
-import http from 'http'
+import { Server } from 'http'
+import dotenv from 'dotenv'
 import socketIO from 'socket.io'
 import { io as connect } from 'socket.io-client'
+import WebSocketService from './services/WebSocketService'
+
+//.dot env init
+dotenv.config({path: path.join(__dirname, '..', '.env')})
 
 //server instance
 const app = express()
-const server = http.Server(app)
+const server = Server(app)
 const io = socketIO(server, {
   cors: {
     origin: '*'
@@ -23,42 +28,17 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 //socket io
-const clientIO = connect('http://localhost:3000')
-let date = new Date()
-let requestQuantity = 0
-
-io.on('connection', socket => {
-  socket.emit('requestQuantity', {
-    requestQuantity,
-    canRequest: requestQuantity < 100
-  })
-})
-
-clientIO.on('updateRequestQuantity', () => {
-  requestQuantity++
-  io.emit('requestQuantity', {
-    requestQuantity,
-    canRequest: requestQuantity < 100
-  })
-})
-
-setInterval(() => {
-  const actualDate = new Date()
-
-  if (actualDate.getDate() != date.getDate()) {
-    date = actualDate
-    requestQuantity = 0
-
-    io.emit('requestQuantity', {
-      requestQuantity,
-      canRequest: requestQuantity < 100
-    })
-  }
-}, 60 * 1000)
+const webSocketService = new WebSocketService(io, connect('http://localhost:3000'))
+webSocketService.start()
 
 //routes
 app.use(router)
 app.use('/pdfs', express.static(path.join(__dirname, 'pdfs')))
 
-export default server
-export { io }
+//server init
+const port = process.env.PORT || 3333
+server.listen(port, () => {
+  console.log(`Server open in port ${port}`)
+})
+
+export default io
